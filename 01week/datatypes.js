7be35d34a5129a3947540b58dedd77775c7ec59b
Justin Essler <justinessler@fastmail.com>
2019-12-05 19:00:32 -0600

    prep for day 2

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -1,1 +1,77 @@
 console.log("Starting Assigment 1");
+
+// Write a JavaScript program to display the current day and time.
+
+let currentDate = new Date();
+console.log("The current date and time is", currentDate);
+
+
+// Write a JavaScript program to convert a number to a string.
+
+var num = 15;
+var n = num.toString();
+console.log("The number is ", num);
+console.log("The string representing the number is ", n);
+
+
+// Write a JavaScript program to convert a string to the number.
+
+var name = "John Doe";
+var age = 12 
+var isMale = true
+console.log("The name variable is a", typeof name);
+
+// Write a JavaScript program that takes in different datatypes and prints out whether they are a:
+// Boolean
+
+var x = 7
+var y = 10
+var z = x+y
+console.log(z)
+console.log("The sum of these datatypes (17) is Boolean,",Boolean (z), "Good Work");
+
+
+
+// Write a JavaScript program that takes in different datatypes and prints out whether they are a:
+// Null
+
+var jimsmith =
+console.log(jimsmith)
+
+
+
+
+// Write a JavaScript program that takes in different datatypes and prints out whether they are a:
+// Undefined
+var d = ''
+console.log("the variable x is", d);
+
+
+// Write a JavaScript program that takes in different datatypes and prints out whether they are a:
+// Number
+
+
+
+
+
+// Write a JavaScript program that takes in different datatypes and prints out whether they are a:
+// NaN
+
+
+
+// Write a JavaScript program that takes in different datatypes and prints out whether they are a:
+// String
+
+
+
+// Write a JavaScript program that adds 2 numbers together.
+
+
+// Write a JavaScript program that runs only when 2 things are true.
+
+
+// Write a JavaScript program that runs when 1 of 2 things are true.
+
+
+// Write a JavaScript program that runs when both things are not true.
+
