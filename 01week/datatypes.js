ddea6a9360db189f1d8be595b75dc5b2306ee96d
Justin E <justin.essler@gmail.com>
2019-12-08 15:40:27 -0600

    committing changes so I can change branches

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -1,77 +1,74 @@
 console.log("Starting Assigment 1");
+// Justin Essler Day #1
 
-// Write a JavaScript program to display the current day and time.
-
+// Write a JavaScript program to display the current day and time.complete
 let currentDate = new Date();
 console.log("The current date and time is", currentDate);
 
-
-// Write a JavaScript program to convert a number to a string.
-
+// Write a JavaScript program to convert a number to a string. complete
 var num = 15;
 var n = num.toString();
 console.log("The number is ", num);
 console.log("The string representing the number is ", n);
 
-
-// Write a JavaScript program to convert a string to the number.
-
+// Write a JavaScript program to convert a string to the number.Complete
 var name = "John Doe";
-var age = 12 
-var isMale = true
-console.log("The name variable is a", typeof name);
+var age = 12;
+var isMale = true;
+console.log("The name variable of John Doe is a", typeof name);
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
-// Boolean
-
-var x = 7
-var y = 10
-var z = x+y
-console.log(z)
-console.log("The sum of these datatypes (17) is Boolean,",Boolean (z), "Good Work");
-
-
+// Boolean, complete
+var x = 7;
+var y = 10;
+var z = x + y;
+console.log(z);
+console.log(
+  "The sum of these datatypes (17) is Boolean,",
+  Boolean(z),
+  "Good Work"
+);
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
 // Null
-
-var jimsmith =
-console.log(jimsmith)
-
-
-
+var guess = null;
+console.log("The name variable of 'guess' is a", typeof guess);
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
-// Undefined
-var d = ''
-console.log("the variable x is", d);
-
+// Undefined, complete
+var d;
+console.log("the variable d is", typeof d);
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
 // Number
-
-
-
-
+var number7 = 15;
+console.log(number7, "is a", typeof number7);
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
 // NaN
 
-
-
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
-// String
+// String, complete
+console.log("The name John Doe from above is a", typeof name);
 
-
-
-// Write a JavaScript program that adds 2 numbers together.
-
+// Write a JavaScript program that adds 2 numbers together. Complete
+var f = 17;
+var d = 26;
+var sumfd = f + d;
+console.log(sumfd);
 
 // Write a JavaScript program that runs only when 2 things are true.
+
+console.log(BoyAge)
+var boy1age = 8;
+var boy2age = 7;
+const myAgeFunc = (boy1age, boy2age) => {
+    if ((boy1age < boy2age) && (boy1age + boy2age >= 15))
+        console.log ("true") } 
+        else {console.log ("false")
+}
 
 
 // Write a JavaScript program that runs when 1 of 2 things are true.
 
-
 // Write a JavaScript program that runs when both things are not true.
-
