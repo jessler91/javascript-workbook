8a6df36046fa6d99b9b8b66195b2cd187bccb4d3
Justin Essler <justinessler@fastmail.com>
2020-01-09 20:51:44 -0600

    saving HOF

---
+++ 04week/loop.js
@@ -0,0 +1,33 @@
+// Week #4 Day #2 HW Assignment
+// Justin Essler - 01/09/2019
+
+// Create a new branch off of master called higherOrderFunctions
+// Use the loops.js in the /04week folder of your workbook.
+// Complete each of the following exercises.
+    // Use a do...while loop to console.log the numbers from 1 to 1000.
+    // Create an object (an array with keys and values) called person with the following data:
+        // firstName: "Jane"
+        // lastName: "Doe"
+        // birthDate: "Jan 5, 1925"
+        // gender: "female"
+    // Use a for...in loop and if statement to console.log the value associated with the key birthDate if the birth year is an odd number.
+// Create an arrayOfPersons that contains mulitiple objects. You can simply copy/paste the person object you made above multiple times. Feel free to change the values to reflect multiple people you might have in your database.
+    // Use .map() to map over the arrayOfPersons and console.log() their information.
+    // Use .filter() to filter the persons array and console.log only males in the array.
+    // Use .filter() to filter the persons array and console.log only people that were born before Jan 1, 1990.
+
+do {
+
+} while (
+    // true or false
+)
+    num > 1000;
+
+
+
+let object = {
+    firstName: "Jane",
+    lastName: "Doe",
+    birthDate: "Jan5, 1925",
+    gender: "female"
+}
---
+++ 04week/week4-notes.js
@@ -0,0 +1,242 @@
+let arr = [1,2,3,3,4,5,6,7,7,7,8,8,8,8,8,9,10]
+
+function removeDuplicate(arr){
+    for (i=0;i > myArray.length; i++) {
+        let result = [];
+        if result.inculdes(arr[i]) {
+            continue;
+        } else {
+            result.push(arr[i]);
+        }
+    } return result;
+}
+
+
+
+// ================== Notes ==================
+// ============== Week #4 ==================
+
+function print (element, index) {
+    console.log(' ${element} is at position ${index}')
+}
+
+function writeLetter(element, index){
+    console.log(`
+        Hi $ {element}
+        It was nice to meet you, here is my info
+        Please reach out
+    `)
+}
+
+let Jack = {
+    Name: "Jack",
+    Role: "Manager",
+    Phone: "555-555-55555" ,
+    email: "jack@gmail.com",
+}
+
+
+let array1 = ['Jack', 'John', 'Justin', "Jared"];
+
+array1.forEach(print);
+
+
+function personalLetter (element, index){
+    console.log(`
+        Hi ${element.name},
+        I understand that you are the ${element.role} a
+    `)
+}
+
+
+// ==============================
+// Notes for Mastermind project
+// ==============================
+
+function exactMatches(array1, array2){
+    return numExact;
+}
+
+function fuzzyMatches(array1, array2) {
+    return numbFuzzy;
+
+}
+
+let s1 = "abcd";
+let guess = "bbbb";
+
+
+console.log(exactMatches(s1, guess));
+console.log(fuzzyMatches(s1, guess));
+
+
+// ================= Week #4 Notes Day #2 ==================
+// WarmUp
+// array of numbers, duplicate, and double
+// video #1
+
+let myArray = [];
+
+let myNewArray = myArray.map((array) => {
+
+})
+
+let doubleIt = myArray.forEach(function(original){
+    let doubled = [];
+    original.forEach(function(element){
+    });
+
+let growIt = function(theArray){
+    let len = theArray.length;
+    for(i=0;i<len;i++) {
+        theArray.push(theArray[i];
+    }
+}
+
+
+// Video #2 Class Recording
+// make array of objects
+// where wach object represents a Credit card transaction
+
+let transactions = [
+    {
+        Date: "01/05/19",
+        Payee: "Target",
+        Amount: "$43.00"
+    },
+    {
+        Date: "01/06/19",
+        Payee: "Wallmart",
+        Amount: "$4.00"
+    },
+    {
+        Date: "01/04/19",
+        Payee: "Cosco",
+        Amount: "$13.00"
+    },
+    {
+        Date: "01/02/19",
+        Payee: "Target",
+        Amount: "$63.00"
+    },
+    {
+        Date: "01/03/19",
+        Payee: "Kohls",
+        Amount: "$90.00"
+    },
+    {
+        Date: "01/07/19",
+        Payee: "Bells",
+        Amount: "$10.00"
+    },
+]
+
+
+transactions.forEach(function(element, index) {
+    console.log("I spent ${element.amount} at ${element.Payee} on ${element.date}.");
+});
+
+
+transactions.forEach(function(element, index) {
+    console.log("I spent ${element.amount} at ${element.Payee} on ${element.date}.");
+});
+
+// Greatest Transactins Reduce =========
+
+let greatest = transactions.reduce(function(prev,current, index){
+    if (prev.Amount > current.Amount) {
+        return prev;
+    } else {
+        return curr;
+    }
+});
+
+console.log("The greatest amt was at ${greatest.payee} for ${greatest.amount}")
+
+
+let discountTransactions = transactions.map(function(element, index){
+    
+    // for every transaction
+    // return a new transactiont hat is identical
+    // except fo a new 10% discount
+
+    let newTransaction  = {};
+    newTransaction.Date = discountTransactions.Date;
+    newTransaction.Payee = discountTransactions.Payee;
+    newTransaction.Amount = discountTransactions.Amount * 9;
+  
+    // let otherTransaction = {
+    //     Date: element.Date,
+    //     Payee: element.Payee,
+    //     Amount: element.Amount*9
+    // }
+    
+    return newTransaction;
+});
+
+discountTransactons.forEach(function(element, index) {
+    console.log("I spent ${element.amount} at ${element.Payee} on ${element.date}.");
+});
+
+
+
+let amounts = transactions.map(function(element, index){
+    return element.Amount;
+});
+
+console.log("The Amounts are = ", amounts)
+
+
+
+// filter is a true false boolean return statement
+let suspicious = amounts.filter(function(element, index){
+    if (element.Amount > 20);
+        return true; 
+    } else {
+        return false;
+    }
+});
+
+
+
+
+
+// const tieDyes = colors.map((color) => {
+//     return `tieDyed-${color}`;
+//   });
+
+
+let names = ["Mike", "Mark", "Matt"];
+let intiials = names.map(function(element, index) {
+    return element[0];
+});
+
+
+console.log(names);
+console.log(initials);
+
+
+
+
+// ============= Reduce ==============
+// something  mention about reduce, the function callback takes three argunments
+// previous, current, and index
+
+
+let nums = [4,3,6,2];
+
+let answer = nums.reduce(function(prev, curr, index){
+    let next = prev + curr;
+    console.log("prev =", prev, "current =", curr, "next =", next);
+    return next;
+
+});
+
+console.log("the answer is ", answer);
+
+
+// ========================= 4.2 Homework =============================
+// google this - loop through the keys of and object in javascript
+// also.........  4.2 HW use forEach() for #5 not .map()
+
+
