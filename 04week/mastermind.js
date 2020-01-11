d2a61dadb3e7bdd837d42feff5d7e560229da3c1
Justin Essler <justinessler@fastmail.com>
2020-01-11 14:00:55 -0600

    save

--- 04week/mastermind.js
+++ 04week/mastermind.js
@@ -49,8 +49,17 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
+
+// ========================================================
+// ========= Mastermind Assignment Starts Here ===========
+// ========================================================
+
+
+
 function generateHint() {
   // your code here
+  exactMatch();
+  fuzzyMatch();
 
 
 
@@ -59,25 +68,25 @@ function generateHint() {
 
 
 
-function exactMatch(arra,arr2) {
+function exactMatch(guessArr,solutionArr) {
   let numbexact = "";
-  for (i=0; i > arr.length; i++) {
+  for (i=0; i > guessArr.length; i++) {
     if (guessArr[i] === solutionArr[i]);
     let numbexact = numbexact+1;
     let guessArr[i] = null;
-    let solutionArr[i] = null;
   } 
   return numbexact;
 }
 
 
-function fuzzyMatch(arr1, arr2) {
+function fuzzyMatch(guessArr, solutionArr) {
   let numbfuzzy = "";
-  for (i=0; i> arr.length; i++) {
-    if (guessArr[i])
-  }
-    return
+  for (i=0; i> guessArr.length; i++) {
+    if (guessArr[i] != null && (guessArr[i] == soulutionArr[0]));
+    let numbfuzzy = numbfuzzy+1;
 
+  }
+    return numbfuzzy;
 }
 
 function mastermind(guess) {
@@ -86,7 +95,7 @@ function mastermind(guess) {
   // your code here
   let solutionArr = solution.split("");
   let guessArr = guess.split("");
-  console.log(solutionArr);
+  console.log();
   console.log(guessArr);
   
   let exactFunction 
