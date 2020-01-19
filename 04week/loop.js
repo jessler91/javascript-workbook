579b619405897170a81ecbda0e31a2655f7b7bbe
jessler91 <justinessler@fastmail.com>
2020-01-18 21:27:59 -0600

    saving loops

--- 04week/loop.js
+++ 04week/loop.js
@@ -1,26 +1,22 @@
 // Justin Essler - 01/09/2019
 // new branch: higherOrderFunctions
-// Use the loops.js in the /04week folder.
+
+// ====================== 4.2 Homework ========================
 // Complete each of the following exercises.
 // google this - "loop through the keys of and object in javascript"
-
-// ========================= 4.2 Homework =============================
-
 // Use a do...while loop to console.log the numbers from 1 to 1000.
 
-let numb = 0;
-do {
-  numb += 1;
-  console.log(numb);
-} while (numb < 1000);
+// let numb = 0;
+// do {
+//   numb += 1;
+//   console.log(numb);
+// } while (numb < 1000);
 
 // Create an object called person
     // firstName: "Jane"
     // lastName: "Doe"
     // birthDate: "Jan 5, 1925"
     // gender: "female"
-// Use a for...in loop and if statement to...
-    // console.log the value associated with the key birthDate if the birth year is an odd number.
 
 let person = {
     firstName: "Jane",
@@ -29,17 +25,15 @@ let person = {
     gender: "female"
 }
 
+// Use a for...in loop and if statement to...
+    // console.log the value associated with the key birthDate if the birth year is an odd number.
+
 
 for (var property in person) {
     if (person.birthDate % 2 === 0) {
-        console.log(key + " -> " + person[key]);
+        console.log(property + " -> " + person[property]);
     }
 }
-
-
-
-
-
 
 
 
@@ -66,12 +60,6 @@ let arrayOfPersons = [
         gender: "female"
     },
     {
-        firstName: "Lady",
-        lastName: "Frizz",
-        birthDate: "Dec 19, 1989",
-        gender: "female"
-    },
-    {
         firstName: "Jane",
         lastName: "Williams",
         birthDate: "Sept 2, 1925",
@@ -88,15 +76,18 @@ arrayOfPersons.forEach(function(index){
 });
 
 // Use .filter() to filter the persons array and console.log only males in the array.
-console.log("======== .filter() ==========");
-arrayOfPersons.filter(function(checkPersonGender);
 
-function checkPersonGender(gender) {
-    return arrayOfPersons.gender === "male";
-}
+console.log("======== .filter(males) ==========");
+arrayOfPersons.filter(function(person){
+    return person.gender === "male";
+});
 
 
 // Use .filter() to filter the persons array and console.log only people that were born before Jan 1, 1990.
+console.log("======== .filter(born before Jan 1, 1990) ==========");
+arrayOfPersons.filter(function seperateDate(){
+    let dateArray = arrayOfPersons.birthDate.split(" ");
+    console.log(dateArray);
 
+});
 
-fumction eightysBaby()
