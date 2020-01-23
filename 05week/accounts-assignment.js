35b49c4d037d2ebaa23ff5158002f452af55e602
jessler91 <justinessler@fastmail.com>
2020-01-23 15:14:03 -0600

    save accounts

--- 05week/accounts-assignment.js
+++ 05week/accounts-assignment.js
@@ -24,7 +24,8 @@
         }
     
         balance() {
-            account.transaction.reduce(function(total, numb){
+            // this method should loop through the list of transactions
+            this.transaction.reduce(function(total, numb){
                 return total + numb;
             })
     
@@ -57,39 +58,92 @@
     //  - and add a new transaction to the list of transactions for the interest accrued
     
     class savingsAccount extends Account {
-        constructor(accountNumber, ownersName, transaction, interestRate) {
+        constructor(accountNumber, ownersName, transaction = [], interestRate) {
             super(accountNumber, ownersName, transaction);
             this.interestRate = interestRate;
         }
     
-        accrueInterest() {
-            // let interstTransaction = new Transaction (interestRate);
-            // let transacton.amount = balance()*.017;
+        addInterestTransaction() {
+            let interestEarned = (this.balance() * interestRate);
+            let newInterestTransaction = new Transaction (payee, interestEarned);
+            this.transaction.push(newInterestTransaction);
             
         }
     
     }
     
     
+    // My working tests
+
+    // create a checking account for Franklin
+    let myKeyAccount = new Account ("xxx-xxx-xca", "Franklin Walters")
+
+
+    // Add a few initial deposits
+    myKeyAccount.addTransaction("deposit#2", 2500);
+    myKeyAccount.addTransaction("deposit#3", 650);
+    // Add a few initial expense transactions
+    myKeyAccount.addTransaction("chickfla", -10.42);
+    myKeyAccount.addTransaction("Kohls", -47.35);
+    myKeyAccount.addTransaction("ATMOS GAS", -37.35);
+
+
+    // Get the balance of myKeyAccount
+    console.log(myKeyAccount.balance());
+
+
+
+
+    
+    // create a checking account for  Savings Walters
+    let mySavingsAccount = new savingsAccount ("xxx-xxx-xsa", "Franklin Savings Walters", transaction = [], .015)
+
+    // Add a few initial savings account deposits
+    mySavingsAccount.addTransaction("savings-deposit-#1", 3500);
+    mySavingsAccount.addTransaction("savings-deposit-#2", 3500);
+    // Add a few initial savings account withdraws
+    mySavingsAccount.addTransaction("savings-withdraw-#1", -300);
+    mySavingsAccount.addTransaction("savings-withdraw-#2", -300);
+
+    // Get the balance of mySavingsAccount
+    mySavingsAccount.balance();
+
+    // Add an interest transaction to mySavingsAccount
+    mySavingsAccount.addInterestTransaction();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    // Yousif example tests
+
+    // let accountA = new Account ("123-345-342", "John Doe");
+    // accountA.addTransaction("Deposit", 1000);
+    // accountA.addTransaction("Target",-45);
+    // accountA.addtransaction("Freebirds", -7.35);
+    // console.log(accountA.balance());  // should print 942.65
+    // accountA.addTransaction("Fraud", -1000);  // this should not go through
+    // console.log(accountA.balance()); // should print 942.65
     
     
-    
-    let accountA = new Account ("123-345-342", "John Doe");
-    accountA.addTransaction("Deposit", 1000);
-    accountA.addTransaction("Target",-45);
-    accountA.addtransaction("Freebirds", -7.35);
-    console.log(accountA.balance());  // should print 942.65
-    accountA.addTransaction("Fraud", -1000);  // this should not go through
-    console.log(accountA.balance()); // should print 942.65
-    
-    
-    let accountB = new Account ("123-345-342", "John Doe");
-    accountB.deposit(1000);
-    accountB.charge(-45);
-    accountB.charge(-7.35);
-    console.log(accountB.balance());  // should print 942.65
-    accountB.charge(-1000);  // this should not go through
-    console.log(accountB.balance()); // should print 942.65
+    // let accountB = new Account ("123-345-342", "John Doe");
+    // accountB.deposit(1000);
+    // accountB.charge(-45);
+    // accountB.charge(-7.35);
+    // console.log(accountB.balance());  // should print 942.65
+    // accountB.charge(-1000);  // this should not go through
+    // console.log(accountB.balance()); // should print 942.65
     
     
     
