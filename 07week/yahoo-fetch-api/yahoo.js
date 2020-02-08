f30a45a7b028d31c76c43f9dbc41f160777fb3ab
jessler91 <justinessler@fastmail.com>
2020-02-08 17:07:15 -0600

    save fetch

--- 07week/yahoo-fetch-api/yahoo.js
+++ 07week/yahoo-fetch-api/yahoo.js
@@ -16,9 +16,80 @@ function getStockData() {
     })
     .then(myjson => {
         console.log(myjson)
+        let industry = summaryProfile.indusrty;
+        let sector = summaryProfile.sector;
+        let website = Object.summaryProfile.website;
+        let totalRevenue = Object.financialData.totalRevenue.fmt;
+        let totalEBITDA = Object.financialData.ebitda.fmt;
+        let grossMargin = Object.financialData.grossMargins.fmt;
+        let totalCash = Object.financialData.totalCash.fmt;
+        let totalDebt = Object.financialData.totalDebt.fmt;
+        let debtTwoEquity = Object.financialData.debtToEquity.fmt;
+
+        console.log(industry);
+
+
+
+
     })
     .catch(err => {
         console.log(err);
     });
+
+
+  
 }
+
+// Define the Important Company Return Metrics
+// Defining the values of the variables collected to be updated
+    let companyName = myjson.Object.price.shortName;
+    let marketCap = Object.price.marketCap.fmt;
+
+    //which of these is correct, above format, or below...?
+
+
+
+
+
+// Get the Element from the HTML by the defined ID
+// set the inner text of the element to be the return metric value
+function updatePage() {
+
+    // Summary Profile
+    let nameElement = document.getElementById("comapnyName");
+    nameElement.innerText = companyName;
+    let marketCapElement = document.getElementById("marketCap");
+    marketCapElement.innerText = marketCap;
+    let industryElement = document.getElementById("industry");
+    industryElement.innerText = industry;
+    let sectorElement = document.getElementById("sector");
+    sectorElement.innerText = sector;
+    let websiteElement = document.getElementById("website");
+    websiteElement.innerText = website;
+
+    //  Income Statement
+    let totalRevenueElement = document.getElementById("totalRevenue");
+    totalRevenueElement.innerText = totalRevenue;
+    let totalEBITDAElement = document.getElementById("ebitda");
+    totalEBITDAElement.innerText = totalEBITDA;
+    let grossMarginElement = document.getElementById("grossMargin");
+    grossMarginElement.innerText = grossMargin;
+
+    //  Balance Sheet Info
+    let totalCashElement = document.getElementById("totalCash");
+    totalCashElement.innerText = totalCash;
+    let totalDebtElement = document.getElementById("totalDebt");
+    totalDebtElement.innerText = totalDebt;
+    let debtTwoEquityElement = document.getElementById("debtTwoEquity");
+    debtTwoEquityElement.innerText = debtTwoEquity;
+
+}
+
+
+
+
+
+
+
+
 
