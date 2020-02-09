5e927cbedded71d1e66618cd61fa424cc4cb64b3
jessler91 <justinessler@fastmail.com>
2020-02-09 12:53:31 -0600

    another save of fetch

--- 07week/yahoo-fetch-api/yahoo.js
+++ 07week/yahoo-fetch-api/yahoo.js
@@ -16,55 +16,44 @@ function getStockData() {
     })
     .then(myjson => {
         console.log(myjson)
-
-        // This is the variable definitions to get the value that 
-        // will be used to update the spad with matching element
-
-
-
-        // it looks like the term is JSON handeling
-        // which of these is correct, above format, or below...?
-        let companyName = myjson.Object.price.shortName;
-        let marketCap = Object.price.marketCap.fmt;
-        let industry = Object.summaryProfile.indusrty;
-        let sector = Object.summaryProfile.sector;
-        let website = Object.summaryProfile.website;
-        let totalRevenue = Object.financialData.totalRevenue.fmt;
-        let totalEBITDA = Object.financialData.ebitda.fmt;
-        let grossMargin = Object.financialData.grossMargins.fmt;
-        let totalCash = Object.financialData.totalCash.fmt;
-        let totalDebt = Object.financialData.totalDebt.fmt;
-        let debtTwoEquity = Object.financialData.debtToEquity.fmt;
-
-        // just tring to see what the returned vaue is 
-        console.log(industry);
-
-
-
-
+        defineVariables(myjson.Object)
     })
+      
     .catch(err => {
         console.log(err);
     });
-
-
   
 }
 
 // Define the Important Company Return Metrics
 // Defining the values of the variables collected to be updated
-
-
+    // This is the variable definitions to get the value that 
+    // will be used to update the spad with matching element
+    // it looks like the term is JSON handeling
+    // which of these is correct, above format, or below...?
     // I moved these variable defining statements UP
     // to inside the console log of the my json promise
 
-    // See above.. line 21
+function defineVariables() {
+    let companyName = Object.price.shortName;
+    let marketCap = Object.price.marketCap.fmt;
+    let industry = Object.summaryProfile.indusrty;
+    let sector = Object.summaryProfile.sector;
+    let website = Object.summaryProfile.website;
+    let totalRevenue = Object.financialData.totalRevenue.fmt;
+    let totalEBITDA = Object.financialData.ebitda.fmt;
+    let grossMargin = Object.financialData.grossMargins.fmt;
+    let totalCash = Object.financialData.totalCash.fmt;
+    let totalDebt = Object.financialData.totalDebt.fmt;
+    let debtTwoEquity = Object.financialData.debtToEquity.fmt;
 
+    // just tring to see what the returned vaue is 
+    console.log(industry);
 
+    // then update the spans with the captured values
+    return updatePage();
 
-
-
-
+}
 
 // Get the Element from the HTML by the defined ID
 // set the inner text of the element to be the return metric value
