9edf79a14e52d17705884a2e5b750bc17cc8e28f
jessler91 <justinessler@fastmail.com>
2020-02-09 12:26:32 -0600

    save address

--- 07week/addressBook/addressBook.js
+++ 07week/addressBook/addressBook.js
@@ -1,21 +1,11 @@
-// Justin Essler - Austin Coding Academy
-// Address Book Assignment
-
-// Read these instructions and understand the requirements FIRST.
-// Whiteboard your app - Make a code plan - Psuedo code and then translate to Javascript
-// Create a new repo (name it descriptively well) and clone it to your local machine and share it with your partner
-// Open the directory and create an index.html and main.js file to fetch a user and display them one at at time.
-// Use the api: https://randomuser.me/api/ to fetch a user now.
-// Fetch a new user multiple times and store them in an array.
-// Then list out all the users in your address book array by name and picture.
-// Figure out how to fetch multiple users in one fetch requests
-// Fetch multiple users on window load.
-// Add a button to each user that when clicked displays the rest of their information like DOB, address and so forth.
-// Once you have the functionality working, feel free to style and structure your address book with CSS and HTML
-// YOU KNOW HOW TO DO ALL OF THIS BY NOW. TRUST YOURSELF!!
-
-// https://randomuser.me/api/
-
+// Justin Essler - Austin Coding Academy -Address Book Assignment
+  // Use the api: https://randomuser.me/api/ to fetch a user.
+  // Fetch a new user multiple times and store them in an array.
+  // Then list out all the users in your address book array by name and picture.
+  // Figure out how to fetch multiple users in one fetch requests
+  // Fetch multiple users on window load.
+  // Add a button to each user that when clicked 
+  // displays the rest of their information like DOB, address, profile image, and so forth.
 
 
 window.onload = function() {
@@ -35,6 +25,7 @@ function getContacts() {
       console.log("processing json payload", myjson);
       displayContacts(myjson.results);
     })
+    
 }
 
 
